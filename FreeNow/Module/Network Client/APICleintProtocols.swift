ac2dea571a604b7fbd0c913a2555cc7f47342864
Omar Tarek <omar.tarek@areebgroup.com>
2021-03-28 11:01:38 +0200

    Added Network fundamentals

---
+++ FreeNow/Module/Network Client/APICleintProtocols.swift
@@ -0,0 +1,36 @@
+//
+//  NetworkClient.swift
+//  FreeNow
+//
+//  Created by Omar Tarek on 3/27/21.
+//
+
+import Foundation
+
+public enum HTTPMethod: String {
+    case options = "OPTIONS"
+    case get     = "GET"
+    case head    = "HEAD"
+    case post    = "POST"
+    case put     = "PUT"
+    case patch   = "PATCH"
+    case delete  = "DELETE"
+    case trace   = "TRACE"
+    case connect = "CONNECT"
+}
+
+typealias Parameters = [String: Any]
+typealias HTTPHeaders = [String: String]
+
+protocol RequestProtocol {
+    var url: String { get }
+    var method: HTTPMethod { get }
+    var headers: HTTPHeaders? { get }
+    var parameters: Parameters? { get }
+}
+
+protocol Model: Codable {}
+
+protocol APICleintProtocol {
+    func send<ResponsType>(request: RequestProtocol, for type: ResponsType.Type ,compeletion: @escaping (Result<ResponsType, Error>) -> Void) where ResponsType: Model
+}

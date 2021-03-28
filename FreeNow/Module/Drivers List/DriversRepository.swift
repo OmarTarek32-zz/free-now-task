b24eb00b5ce405c25a0ebf9c3666b2f35f90bae7
Omar Tarek <omar.tarek@areebgroup.com>
2021-03-28 22:39:21 +0200

    Added DriversRepository Modifed APIClient Protocol and its implmentation

--- FreeNow/Module/Drivers List/DriversRepository.swift
+++ FreeNow/Module/Drivers List/DriversRepository.swift
@@ -9,4 +9,29 @@ import Foundation
 
 protocol DriversRepositoryProtocol {
 
+    var apiClient: APICleintProtocol { get }
+    
+    func getDriversList(topLeftPointLat: Double, topLeftPointLong: Double, rightBottomPointLat: Double, rightBottomPointLong: Double, compeletion: @escaping (Result<Drivers, Error>) -> Void)
+}
+
+struct DriversRepository {
+    
+    var apiClient: APICleintProtocol
+}
+
+extension DriversRepository: DriversRepositoryProtocol {
+    
+    func getDriversList(topLeftPointLat: Double,
+                        topLeftPointLong: Double,
+                        rightBottomPointLat: Double,
+                        rightBottomPointLong: Double,
+                        compeletion: @escaping (Result<Drivers, Error>) -> Void) {
+        
+        let request = DriversRequest.list(leftTopPointLat: topLeftPointLat,
+                                          leftTopPointLong: topLeftPointLong,
+                                          rightBottomPointLat: rightBottomPointLat,
+                                          rightBottomPointLong: rightBottomPointLong)
+        
+        apiClient.send(request: request,compeletion: compeletion)
+    }
 }

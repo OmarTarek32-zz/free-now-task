f1674b51cf504d6c604e2fad481045361abaafee
Omar Tarek <omar.tarek@areebgroup.com>
2021-03-29 00:20:27 +0200

    Added MapViewController

---
+++ FreeNow/Module/Drivers List/View/Drivers Map View /DriversMapViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DriversMapViewController.swift
+//  FreeNow
+//
+//  Created by Omar Tarek on 3/29/21.
+//
+
+import UIKit
+
+class DriversMapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
